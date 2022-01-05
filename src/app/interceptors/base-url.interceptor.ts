4461b662bf35d6c77b2c671716d811b59b00d617
Anderson Freitas <anderson.santos@willbank.com.br>
2022-01-05 15:53:25 -0300

    using environment variables

--- src/app/interceptors/base-url.interceptor.ts
+++ src/app/interceptors/base-url.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
@@ -13,11 +14,15 @@ export class BaseUrlInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (!request.url.startsWith('http')) {
-      request = request.clone({
-        url: 'https://anderson.free.mockoapp.net/' + request.url
-      });
+    if (request.url.startsWith('/')) {
+      throw new Error('Url must not start with "/" to follow our convention');
     }
+
+    if (!request.url.startsWith('http')) {
+      const url = environment.apiUrl + request.url;
+      request = request.clone({ url });
+    }
+
     return next.handle(request);
   }
 }
