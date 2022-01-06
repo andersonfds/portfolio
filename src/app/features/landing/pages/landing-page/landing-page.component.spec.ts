c21a365bc92084cccda7bb6c8bddec002febc657
Anderson Freitas <anderson.santos@willbank.com.br>
2022-01-06 11:12:56 -0300

    feat: unit testing the landing component

--- src/app/features/landing/pages/landing-page/landing-page.component.spec.ts
+++ src/app/features/landing/pages/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
+import { ProfileModel } from '../../data/model/profile.model';
+import { ProjectResumeModel } from '../../data/model/project_resume.model';
 import { LandingService } from '../../data/remote/landing.service';
 
 import { LandingPageComponent } from './landing-page.component';
@@ -8,12 +10,21 @@ import { LandingPageComponent } from './landing-page.component';
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
+  let projectResumes: ProjectResumeModel[];
+  let profile: ProfileModel;
 
   beforeEach(async () => {
     let mockLandingService = jasmine.createSpyObj('LandingService', ['getProjectsResume', 'getProfile']);
+    projectResumes = [];
+    profile = {
+      name: 'John Doe',
+      email: 'john@doe.com',
+      avatar: 'https://picsum.photos/seed/johndoe/64/64',
+      social: [],
+    };
 
-    mockLandingService.getProjectsResume.and.returnValue(of([]));
-    mockLandingService.getProfile.and.returnValue(of({}));
+    mockLandingService.getProjectsResume.and.returnValue(of(projectResumes));
+    mockLandingService.getProfile.and.returnValue(of(profile));
 
     await TestBed.configureTestingModule({
       declarations: [ LandingPageComponent ],
@@ -35,5 +46,12 @@ describe('LandingPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the email', () => {
+    const rawHtml = fixture.nativeElement as HTMLElement;
+    const element = rawHtml.querySelector('.profile-email')
+    expect(element).toBeTruthy();
+    expect(element!.textContent).toBe(profile.email);
   });
 });
