fc6a94934d354166bc905323249b490239efe863
Anderson Freitas <anderson.santos@willbank.com.br>
2022-01-10 10:14:11 -0300

    feat: Automatic carousel for projects

--- src/app/features/landing/pages/landing-page/landing-page.component.ts
+++ src/app/features/landing/pages/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of, shareReplay } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, OnDestroy } from '@angular/core';
+import { BehaviorSubject, first, firstValueFrom, interval, map, Observable, of, shareReplay, Subscription, switchMap, tap } from 'rxjs';
 import { ProfileModel } from '../../data/model/profile.model';
 import { MediaType, ProjectResumeModel, ProjectType } from '../../data/model/project_resume.model';
 import { LandingService } from '../../data/remote/landing.service';
@@ -8,16 +9,58 @@ import { LandingService } from '../../data/remote/landing.service';
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit, OnDestroy {
+
+  private projectSelector = new BehaviorSubject<ProjectResumeModel | null>(null);
+  private subscriptions = new Array<Subscription>();
 
   // observables
   public projects$: Observable<ProjectResumeModel[]> = this.landingService.getProjectsResume();
   public profile$: Observable<ProfileModel> = this.landingService.getProfile().pipe(shareReplay());
   public date$: Observable<Date> = of(new Date());
+  public projectMargin$!: Observable<number>;
 
   // public enumerators
   public mediaTypes = MediaType;
   public projectTypes = ProjectType;
 
-  constructor(private readonly landingService: LandingService) { }
+  constructor(private readonly landingService: LandingService, @Inject(DOCUMENT) private document: Document) { }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe()); 
+  }
+
+  ngAfterViewInit(): void {
+    this.projectMargin$ = this.projects$.pipe(
+      first(),
+      map((values) => values[0]),
+      tap((value) => this.projectSelector.next(value)),
+      switchMap(() => this.projectSelector.asObservable()),
+      map(project => -1 * (this.document.getElementById('proj--' + project?.id)?.offsetLeft || 0)),
+    );
+    this.subscriptions.push(interval(5000).subscribe(() => this.nextProject()));
+  }
+
+  async nextProject() {
+    const nextProject = await this.getNextProject();
+
+    if (nextProject)
+      this.projectSelector.next(nextProject);
+  }
+
+  getNextProject(): Promise<ProjectResumeModel | undefined> {
+    return firstValueFrom(this.projects$
+      .pipe(
+        first(),
+        map(projects => {
+          const currentProjectIndex = projects.findIndex(project => project.id === this.projectSelector.value?.id);
+
+          if (currentProjectIndex === -1) {
+            return projects[0];
+          }
+
+          return projects[currentProjectIndex + 1] || projects[0];
+        }),
+      ));
+  }
 }
